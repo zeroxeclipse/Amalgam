52619df35ed265225535648b8661a5566d5a01d6
Riley <Riley@trigon.systems>
2024-05-27 21:35:50 -0500

    Fix a crash relating to SetAlphaModulation

--- Amalgam/src/Hooks/StudioRender_SetAlphaModulation.cpp
+++ Amalgam/src/Hooks/StudioRender_SetAlphaModulation.cpp
@@ -3,8 +3,20 @@
 MAKE_HOOK(StudioRender_SetAlphaModulation, U::Memory.GetVFunc(I::StudioRender, 28), void, __fastcall,
 	void* ecx, float flAlpha)
 {
-	if (Vars::Visuals::World::Modulations.Value & (1 << 2) && G::DrawingProps && !(Vars::Visuals::UI::CleanScreenshots.Value && I::EngineClient->IsTakingScreenshot()))
-		return CALL_ORIGINAL(ecx, float(Vars::Colors::PropModulation.Value.a) / 255.f * flAlpha);
+	if(Vars::Visuals::World::Modulations.Value & (1 << 2) && 
+		G::DrawingProps && 
+		!(Vars::Visuals::UI::CleanScreenshots.Value && I::EngineClient->IsTakingScreenshot()))
+	{
+		auto flVal = Vars::Colors::PropModulation.Value.a / 255.f * flAlpha;
+
+		if(flVal == 0.f)
+		{
+			flVal = 1.f;
+		}
+
+		return CALL_ORIGINAL(ecx, flVal);
+	}
+		
 
 	CALL_ORIGINAL(ecx, flAlpha);
 }
